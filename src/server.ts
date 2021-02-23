3858ed9fea21e259ffd9fbcb17db0622c1debd8f
LuigiRamires <54213955+LuigiRamires@users.noreply.github.com>
2021-02-23 17:00:29 -0300

    Day 02

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,11 @@
+import 'reflect-metadata'
 import express, { response } from 'express';
+import './database';
+import { router } from './routes';
 
 const app = express();
 
-/*
- * GET => Busca
- * POST => Salvar
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Alteração específica
- */
-
-// http://localhost:3333/users
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World - NLW04" });
-})
-
-// 1 parâmetro => Rota(Recurso API)
-// 2 parâmetro => request, response
-
-app.post("/", (request, response) => {
-  // Recebeu os dados para salvar
-  return response.json({ message: "Os dados foram salvos corretamente." });
-});
+app.use(express.json());
+app.use(router);
 
 app.listen(3333, () => console.log("Server is running!"));
